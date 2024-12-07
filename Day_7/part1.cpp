65edbb4ac21a76cc58fe2e972d29a3db6bd59d9e
miles352 <milestdaly@icloud.com>
2024-12-07 00:47:10 -0500

    Day 7 complete

---
+++ Day_7/part1.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+bool calibrate(long long target, std::vector<long long> values, long long total) {
+    if (values.empty()) return target == total;
+
+    // if + or * is true, return true
+    std::vector<long long> new_values(values.begin() + 1, values.end());
+    if (calibrate(target, new_values, total + values[0]) || calibrate(target, new_values, total * values[0])) return true;
+    return false;
+}
+
+int main() {
+    std::ifstream file("../Day_7/input.txt");
+    std::string str;
+
+    long long total = 0;
+
+    while (std::getline(file, str)) {
+        int seperator = str.find(':');
+        long long target = std::stoll(str);
+        std::vector<long long> values;
+        std::string temp = "";
+        for (int i = seperator + 2; i < str.length(); i++) {
+            if (str[i] == ' ') {
+                values.push_back(std::stoll(temp));
+                temp = "";
+            } else {
+                temp += str[i];
+            }
+        }
+        values.push_back(std::stoll(temp));
+
+        std::vector<long long> new_values(values.begin() + 1, values.end());
+        if (calibrate(target, new_values, values[0])) total += target;
+    }
+
+    std::cout << total << std::endl;
+
+}
